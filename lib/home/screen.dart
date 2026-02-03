58297fde91f6904edc13782c1e800c6c40a8a0eb
Faria Ahmed <fariaahmed424@gmail.com>
2026-02-03 23:05:22 +0600

    update

--- lib/home/screen.dart
+++ lib/home/screen.dart
@@ -24,30 +24,38 @@ class _HscreenState extends State<Hscreen> {
         centerTitle: true,
         titleTextStyle: TextStyle(color: Colors.white),
       ),
+
       // body: ElevatedButton(
       //   onPressed: () {
       //     changes();
       //   },
       //   child: Text('button'),
       // ),
-      body: Text(
-        'hello everyone , how are you?',
-        maxLines: 3,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontFamily: 'myFont',
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          fontStyle: FontStyle.normal,
-          letterSpacing: 2.0,
-          backgroundColor: Colors.green,
-          color: Colors.white70,
-          decorationColor: Colors.black38,
-          decoration: TextDecoration.overline,
-          shadows: [
-            Shadow(color: Colors.black, blurRadius: 5, offset: Offset(-5, -10)),
-          ],
-        ),
+      // body: Text(
+      //   'hello everyone , how are you?',
+      //   maxLines: 3,
+      //   textAlign: TextAlign.center,
+      //   style: TextStyle(
+      //     fontFamily: 'myFont',
+      //     fontSize: 30,
+      //     fontWeight: FontWeight.bold,
+      //     fontStyle: FontStyle.normal,
+      //     letterSpacing: 2.0,
+      //     backgroundColor: Colors.green,
+      //     color: Colors.white70,
+      //     decorationColor: Colors.black38,
+      //     decoration: TextDecoration.overline,
+      //     shadows: [
+      //       Shadow(color: Colors.black, blurRadius: 5, offset: Offset(-5, -10)),
+      //     ],
+      //   ),
+      // ),
+      body: Image(
+        image: AssetImage('assets/images/1.jpg'),
+        height: 100,
+        width: 50,
+        fit: BoxFit.cover,
+        alignment: Alignment.center,
       ),
     );
   }
