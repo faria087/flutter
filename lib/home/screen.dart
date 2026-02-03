02b520fda364f3ef7e522ae8b37c770c30d72c6b
Faria Ahmed <fariaahmed424@gmail.com>
2026-02-03 22:15:55 +0600

    Initial commit

---
+++ lib/home/screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Hscreen extends StatefulWidget {
+  const Hscreen({super.key});
+
+  @override
+  State<Hscreen> createState() => _HscreenState();
+}
+
+class _HscreenState extends State<Hscreen> {
+  String title = 'welcome to my app';
+  void changes() {
+    setState(() {
+      title = 'welcome home';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Text(title),
+        centerTitle: true,
+        titleTextStyle: TextStyle(color: Colors.white),
+      ),
+      // body: ElevatedButton(
+      //   onPressed: () {
+      //     changes();
+      //   },
+      //   child: Text('button'),
+      // ),
+      body: Text(
+        'hello everyone , how are you?',
+        maxLines: 3,
+        style: TextStyle(
+          fontFamily: 'myFont',
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.normal,
+          letterSpacing: 2.0,
+          backgroundColor: Colors.green,
+          color: Colors.white70,
+          decorationColor: Colors.black38,
+          decoration: TextDecoration.overline,
+          shadows: [
+            Shadow(color: Colors.black, blurRadius: 5, offset: Offset(-5, -10)),
+          ],
+        ),
+      ),
+    );
+  }
+}
