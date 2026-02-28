ac1f23819bf2507811f2d30e581cb32af12d4230
Faria Ahmed <fariaahmed424@gmail.com>
2026-03-01 01:07:59 +0600

    login validation +rsponsive

--- lib/home/login.dart
+++ lib/home/login.dart
@@ -8,53 +8,93 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _formkey = GlobalKey<FormState>();
+  final email = TextEditingController();
+  final password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
-            FlutterLogo(size: 50),
-            Text(
-              'Please login first to get started.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  FlutterLogo(size: 50),
+                  SizedBox(height: 20),
+                  Text(
+                    'Please login first to get started.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: email,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'please enter your email';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: password,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'please enter your email';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {}
+                    },
+                    child: Text('login'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      textStyle: TextStyle(color: Colors.white, fontSize: 20),
+                      padding: EdgeInsets.all(15),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('Forgot password?'),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(
+                    width: double.infinity,
+                    height: 30,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text("don't have a account? Register Now"),
+                    ),
+                  ),
+                ],
               ),
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('login'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueGrey,
-                  textStyle: TextStyle(color: Colors.white, fontSize: 10),
-                ),
-              ),
-            ),
-            TextButton(onPressed: () {}, child: Text('Forgot password?')),
-            Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: OutlinedButton(
-                onPressed: () {},
-                child: Text("don't have a account? Register Now"),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
